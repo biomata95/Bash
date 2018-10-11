e0d21a1776471960a58177cf8b1377e402bca3f5
biomata95 <37086825+biomata95@users.noreply.github.com>
2018-10-11 12:10:14 -0300

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,6 +1,14 @@
 #arp-scan --interface=enp2s0f5 --localnet
 
+
+nmap 192.168.1.3
+
+
+
 #!/bin/bash
-for ip in 192.168.0.{1..254}; do
-  ping -c 1 -W 1 $ip | grep "64 bytes" &
-done
+#for ip in 192.168.0.{1..254}; do
+#  ping -c 1 -W 1 $ip | grep "64 bytes" &
+#done
+
+
+
